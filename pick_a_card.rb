462a4bd1497834578a6d05d2e4142cb74690602c
Ivan <115182606+IvanDBS@users.noreply.github.com>
2022-10-28 09:55:18 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -10,5 +10,5 @@ end
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 suits = ['♦', '♠', '♥ ', '♣']
-
-puts "#{values.sample}#{suits.sample}"
+puts 
+puts "Your random card is => #{values.sample}#{suits.sample}"
